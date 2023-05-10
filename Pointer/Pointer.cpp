394016d06d10b73d7eac0633a30019148a119ef6
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-10 11:05:57 +0700

    membuat object, pointer reference, member access, pointer dereference mahasiswa

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -11,3 +11,19 @@ public:
 void mahasiswa::showNim() {		//implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 };			//object mhs
+	mhs.showNim();				//Member Acces operator
+
+	mahasiswa& ref = mhs;		//pointer reference refMhs
+	ref.nim = 2;				//Member access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		//Pointer Dereference pMhs
+	pMhs->nim = 3;				//Arrow opertaror
+	pMhs->showNim();
+	system("pause");
+	return 0;
+}
