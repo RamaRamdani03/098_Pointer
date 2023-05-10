eb23c077c856bec573952a3f5a7e795038d39c78
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-10 12:10:05 +0700

    menampilkan nilai pelajar

--- FriendClass3/FriendClass3.cpp
+++ FriendClass3/FriendClass3.cpp
@@ -22,3 +22,12 @@ public:
 void manusia::showNilaipelajar(pelajar& x) {
 	cout << "Nilai Pelajar: " << x.nilai;
 }
+int main()
+{
+	manusia m;
+	pelajar p;
+	m.showNilaipelajar(p);
+	cout << endl;
+	system("\npause");
+	return 0;
+}
